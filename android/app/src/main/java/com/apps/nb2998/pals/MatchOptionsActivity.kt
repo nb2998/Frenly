1cafd450098bddfe076ce15c193b854d1c7fd6a8
Nishtha Bhatia <nbhatia329@gmail.com>
2019-03-30 09:05:26 +0530

    Init+Match options

---
+++ android/app/src/main/java/com/apps/nb2998/pals/MatchOptionsActivity.kt
@@ -0,0 +1,12 @@
+package com.apps.nb2998.pals
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MatchOptionsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_match_options)
+    }
+}
