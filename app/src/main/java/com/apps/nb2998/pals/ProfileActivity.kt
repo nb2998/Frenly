49417242100f75479341edd7ed0e0ce2d6485a78
Nishtha Bhatia <nbhatia329@gmail.com>
2019-03-30 11:40:08 +0530

    Match options activity

---
+++ app/src/main/java/com/apps/nb2998/pals/ProfileActivity.kt
@@ -0,0 +1,21 @@
+package com.apps.nb2998.pals
+
+import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_profile.*
+
+class ProfileActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile)
+        setSupportActionBar(toolbar)
+        fab.setOnClickListener { view ->
+//            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                .setAction("Action", null).show()
+        }
+
+
+    }
+}
